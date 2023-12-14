84fc2ac2d07f2aa8efd6de3ec0e243e565b05732
Lang <zou2396154320@gmail.com>
2023-12-14 19:15:40 +0800

    新增查询车辆详情

--- src/com/car/controller/GetDetailController.java
+++ src/com/car/controller/GetDetailController.java
@@ -1,6 +1,8 @@
 package com.car.controller;
 
+import com.car.dao.impl.CarDao;
 import com.car.dao.impl.StaffDao;
+import com.car.entity.Car;
 import com.car.entity.Staff;
 import com.car.result.Result;
 import com.car.utils.Utils;
@@ -16,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 public class GetDetailController extends HttpServlet {
 
   /**
-   * 查询车辆详细
+   * 查询员工详细
    *
    * @param req
    * @param resp
@@ -42,12 +44,13 @@ public class GetDetailController extends HttpServlet {
       Staff staff = new StaffDao().selectById(id);
       Utils.returnJson(resp, Result.success(staff));
     } catch (Exception e) {
+      Utils.returnJson(resp,Result.error("查询失败"));
       e.printStackTrace();
     }
   }
 
   /**
-   * 查询员工详细
+   * 查询车辆详细
    *
    * @param req
    * @param resp
@@ -57,6 +60,24 @@ public class GetDetailController extends HttpServlet {
   @Override
   protected void doPost(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-    super.doPost(req, resp);
+    // 设置编码格式
+    req.setCharacterEncoding("UTF-8");
+    resp.setCharacterEncoding("UTF-8");
+
+    // 解析请求
+    //        Staff staff = JSON.parseObject(Utils.parse(req), Staff.class);
+
+    // 设置响应头的编码
+    resp.setHeader("Content-Type", "application/json;charset=utf-8");
+
+    String id = req.getParameter("id");
+
+    try {
+      Car car = new CarDao().selectById(id);
+      Utils.returnJson(resp, Result.success(car));
+    } catch (Exception e) {
+      Utils.returnJson(resp,Result.error("查询失败"));
+      e.printStackTrace();
+    }
   }
 }
